9a5d6b5a6277a4f02de454e275fba743d65c0b9e
Nathan Hare <nhare330@gmail.com>
2016-01-22 15:35:59 -0500

    Begin building foundation

---
+++ js/main.js
@@ -0,0 +1,11 @@
+angular.module('storygenApp', [])
+  .controller('storygenCon', function() {
+    var storygen = this;
+    storygen.number = 9;
+    storygen.dice = [];
+
+    //FUNCTIONS
+    
+
+
+  });
